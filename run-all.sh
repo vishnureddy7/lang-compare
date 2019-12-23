89bd8f63bc731f6fd2ec40b88ae55ab82346364f
Guru Vishnu Vardhan Reddy Desireddy <vishnuvardhan12137@gmail.com>
2019-12-23 17:26:17 +0530

    Added format to run-all.sh script

--- run-all.sh
+++ run-all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
-echo "To run 10M instructions"
-echo -n "C "
-gcc loop.c -o cloop
+printf "To run 10M instructions\n"
+printf "Language\tTime Taken(s)\n"
+printf "C\t\t"
+gcc loop.c -o cloop.out
 (time ./cloop) |& grep real | cut -b 8-12
-echo -n "C++ "
-g++ loop.cpp -o cpploop
+printf "C++\t\t"
+g++ loop.cpp -o cpploop.out
 (time ./cpploop) |& grep real | cut -b 8-12
-echo -n "Java "
+printf "Java\t\t"
 javac JLoop.java
 (time java JLoop) |& grep real | cut -b 8-12
-echo -n "Python "
+printf "Python\t\t"
 (time python3 loop.py) |& grep real | cut -b 8-12
-echo -n "Javascript "
+printf "Javascript\t"
 (time node loop.js) |& grep real | cut -b 8-12
-echo -n "Erlang "
+printf "Erlang\t\t"
 erl -noshell -eval 'loop:run(start),init:stop().' | cut -b -5
